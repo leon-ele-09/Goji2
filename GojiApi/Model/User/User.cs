049d57bcff5fbb24d1b0ca51a58bd708f25eba88
luigicast <143055612+luigicast@users.noreply.github.com>
2026-09-09 13:09:42 -0700

    User Model Added

--- GojiApi/Model/User/User.cs
+++ GojiApi/Model/User/User.cs
@@ -1,7 +1,40 @@
-﻿namespace GojiApi.Model.User
+﻿using System.Runtime.Serialization;
+
+namespace GojiApi.Model.User
 {
     public class User
     {
-// calando pull
+        public Guid Id { get; private set; }
+        public string Name { get; set; }
+        public string Email { get; set; } = null!;
+        public string PasswordHash { get; set; } = null!;
+        public bool Active { get; set; }
+        public DateTime? CreatedAt { get; private set; }
+
+        public User() { }
+
+        public static User Create(string name) => new User 
+        { 
+            Name = name,
+            Id = Guid.NewGuid(),
+            CreatedAt = DateTime.UtcNow,
+            Active = true
+        };
+
+        public User WithEmail(string email)
+        {
+            Email = email;
+            return this;
+        }
+
+        public User WithPasswordHash(string passwordHash)
+        {
+            PasswordHash = passwordHash;
+            return this;
+        }
+
+        public User Activate() { Active = true; return this; }
+
+        public User Deactivate() { Active = false; return this; }
     }
 }
