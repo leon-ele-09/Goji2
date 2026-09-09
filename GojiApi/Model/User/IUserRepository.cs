26a7e2cedded15f0c6e53e40f50d39420bc5d1f8
luigicast <143055612+luigicast@users.noreply.github.com>
2026-09-09 13:13:46 -0700

    User Interface Repository

---
+++ GojiApi/Model/User/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace GojiApi.Model.User
+{
+    public interface IUserRepository
+    {
+        void Add(User user);
+        User GetById(Guid id);
+        User GetByName(string name);
+        User GetByEmail(string email);
+        void Update(User user);
+        void Delete(User user);
+    }
+}
