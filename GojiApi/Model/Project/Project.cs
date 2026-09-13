e2327d6c1582ad2bf6bf89a73ef4663c20368750
luigicast <143055612+luigicast@users.noreply.github.com>
2026-09-12 20:13:16 -0700

    More Attributes Added

--- GojiApi/Model/Project/Project.cs
+++ GojiApi/Model/Project/Project.cs
@@ -1,33 +1,58 @@
 ﻿namespace GojiApi.Model.Project
 {
-    public class Project : IProject
+    public class Project
     {
+        public Guid Id { get; set; }
+        public string? BusinessKey { get; set; }
         public string? Name { get; set; }
-        public List<int> UserIds { get; set; } = new();
+        public List<Guid> UserIds { get; set; } = new();
+        public DateTime? CreatedAt { get; private set; }
 
-        public static Project Create()
+        public static Project Create() => new Project
         {
-            return new Project();
+            Id = Guid.NewGuid(),
+            CreatedAt = DateTime.Now
+        };
+
+        public Project WithName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name)) { throw new ArgumentException("Invalid Name"); }
+
+            Name = name;
+            return this;
         }
 
-        public Project AssignUser(int userId)
+        public Project WithBusinessKey(string businessKey)
         {
-            if (!UserIds.Contains(userId))
+            if (string.IsNullOrWhiteSpace(businessKey)) { throw new ArgumentException("Invalid BusinessKey"); }
+
+            BusinessKey = businessKey;
+            return this;
+        }
+
+        public bool HasUser(Guid Id)
+        {
+            return UserIds.Contains(Id);
+        }
+
+        public Project AddUser(Guid Id)
+        {
+            if (!UserIds.Contains(Id))
             {
-                UserIds.Add(userId);
+                UserIds.Add(Id);
             }
             return this;
         }
 
-        public Project RemoveUser(int userId)
+        public Project RemoveUser(Guid Id)
         {
-            UserIds.Remove(userId);
+            if (UserIds.Contains(Id))
+            {
+                UserIds.Remove(Id);
+            }
             return this;
         }
 
-        public bool HasUser(int userId)
-        {
-            return UserIds.Contains(userId);
-        }
+        
     }
 }
