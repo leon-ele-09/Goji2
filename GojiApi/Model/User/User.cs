2df48f75b0aa9c51d5e4487ad8ffa7182e616210
luigicast <143055612+luigicast@users.noreply.github.com>
2026-09-12 20:30:41 -0700

    Name Assignment Corrected

--- GojiApi/Model/User/User.cs
+++ GojiApi/Model/User/User.cs
@@ -5,17 +5,14 @@ namespace GojiApi.Model.User
     public class User
     {
         public Guid Id { get; private set; }
-        public string Name { get; set; }
-        public string Email { get; set; } = null!;
+        public string? Name { get; set; }
+        public string? Email { get; set; } = null!;
         // public string PasswordHash { get; set; } = null!;
         public bool Active { get; set; }
         public DateTime? CreatedAt { get; private set; }
 
-        public User() { }
-
         public static User Create(string name) => new User 
         { 
-            Name = name,
             Id = Guid.NewGuid(),
             CreatedAt = DateTime.UtcNow,
             Active = true
