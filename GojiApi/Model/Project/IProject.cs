298d8b8a54f5c40ca95bd7b09bafd784623bc682
luigicast <143055612+luigicast@users.noreply.github.com>
2026-09-12 20:23:50 -0700

    Project Interface Functions

--- GojiApi/Model/Project/IProject.cs
+++ GojiApi/Model/Project/IProject.cs
@@ -2,12 +2,11 @@
 {
     public interface IProject
     {
-        global::System.String Name { get; set; }
-        List<global::System.Int32> UserIds { get; set; }
-
-        static abstract Project Create();
-        Project AssignUser(global::System.Int32 userId);
-        global::System.Boolean HasUser(global::System.Int32 userId);
-        Project RemoveUser(global::System.Int32 userId);
+        void Add(Project project);
+        Project GetById(Guid id);
+        Project GetByName(string name);
+        Project GetByBusinessKey(string businessKey);
+        void Update(Project project);
+        void Delete(Project project);
     }
 }
