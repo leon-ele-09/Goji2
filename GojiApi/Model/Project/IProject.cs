6386344d490a1c5d351e373c70336901af4df09a
CrisA05 <cristobal.arminan@cetys.edu.mx>
2026-09-12 18:27:51 -0700

    Project class and interface

---
+++ GojiApi/Model/Project/IProject.cs
@@ -0,0 +1,13 @@
+﻿namespace GojiApi.Model.Project
+{
+    public interface IProject
+    {
+        global::System.String Name { get; set; }
+        List<global::System.Int32> UserIds { get; set; }
+
+        static abstract Project Create();
+        Project AssignUser(global::System.Int32 userId);
+        global::System.Boolean HasUser(global::System.Int32 userId);
+        Project RemoveUser(global::System.Int32 userId);
+    }
+}
--- GojiApi/Model/Project/Project.cs
+++ GojiApi/Model/Project/Project.cs
@@ -1,6 +1,33 @@
 ﻿namespace GojiApi.Model.Project
 {
-    public class Project
+    public class Project : IProject
     {
+        public string? Name { get; set; }
+        public List<int> UserIds { get; set; } = new();
+
+        public static Project Create()
+        {
+            return new Project();
+        }
+
+        public Project AssignUser(int userId)
+        {
+            if (!UserIds.Contains(userId))
+            {
+                UserIds.Add(userId);
+            }
+            return this;
+        }
+
+        public Project RemoveUser(int userId)
+        {
+            UserIds.Remove(userId);
+            return this;
+        }
+
+        public bool HasUser(int userId)
+        {
+            return UserIds.Contains(userId);
+        }
     }
 }
