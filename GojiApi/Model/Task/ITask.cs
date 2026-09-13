1c334e31fce17055741b39a227c7d3f22224ec26
CrisA05 <cristobal.arminan@cetys.edu.mx>
2026-09-12 17:09:09 -0700

    Task class and interface

---
+++ GojiApi/Model/Task/ITask.cs
@@ -0,0 +1,10 @@
+﻿namespace GojiApi.Model.Task
+{
+    public interface ITask
+    {
+        int? UserId { get; set; }
+
+        static abstract Task Create();
+        Task AssignUser(int userId);
+    }
+}
--- GojiApi/Model/Task/Task.cs
+++ GojiApi/Model/Task/Task.cs
@@ -1,6 +1,27 @@
 ﻿namespace GojiApi.Model.Task
 {
-    public class Task
+    public class Task : ITask
     {
+        public int? UserId { get; set; }
+
+        public static Task Create()
+        {
+            return new Task();
+        }
+
+        public Task AssignUser(int userId)
+        {
+            UserId = userId;
+            return this;
+        }
+
+
+        /* Uso
+         * 
+         * Task task = Task.Create()
+         * 
+         * task
+         *  .AssignUser(15);
+         */
     }
 }
