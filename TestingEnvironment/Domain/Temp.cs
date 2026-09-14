f908802fbb3b443e1f54bbaafcac6594f6226aaf
luigicast <143055612+luigicast@users.noreply.github.com>
2026-09-13 19:04:50 -0700

    Temp Test Added

---
+++ TestingEnvironment/Domain/Temp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestingEnvironment.TestCommon;
+
+namespace TestingEnvironment.Domain
+{
+    [TestFixture]
+    public class BuilderSanityTests
+    {
+        [Test]
+        public void UserBuilder_Build_ReturnsValidUser()
+        {
+            var user = new UserBuilder().WithName("Carlos").Build();
+
+            Assert.That(user.Name, Is.EqualTo("Carlos"));
+            Assert.That(user.Active, Is.True);
+        }
+
+        [Test]
+        public void ProjectBuilder_WithUsers_AddsAllUsers()
+        {
+            var u1 = Guid.NewGuid();
+            var u2 = Guid.NewGuid();
+            var project = new ProjectBuilder().WithUsers(u1, u2).Build();
+
+            Assert.That(project.HasUser(u1), Is.True);
+            Assert.That(project.HasUser(u2), Is.True);
+        }
+
+        [Test]
+        public void TaskItemBuilder_ForProject_SetsProjectId()
+        {
+            var project = new ProjectBuilder().Build();
+            var task = new TaskItemBuilder().ForProject(project).Build();
+
+            Assert.That(task.ProjectId, Is.EqualTo(project.Id));
+        }
+    }
+}
